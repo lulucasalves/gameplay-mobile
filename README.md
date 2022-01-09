67021fcfd3906f708a32e172a28ba1889d1187f5
lulucas <lucas.alves.supus@outlook.com>
2022-01-09 16:12:03 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+<h1>GamePlay</h1>
---
+++ src/@types/svg.d.ts
@@ -0,0 +1,8 @@
+import React from 'react'
+
+declare module '*.svg' {
+  import React from 'react'
+  import { SvgProps } from 'react-native-svg'
+  const content: React.FC<SvgProps>
+  export default content
+}
